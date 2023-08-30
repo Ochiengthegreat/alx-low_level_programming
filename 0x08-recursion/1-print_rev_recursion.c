2a6bc979757de7273ac8e7b55f6cd0e4daf8f95d
Ochiengthegreat <ochiengroy49@gmail.com>
2023-08-30 19:28:14 +0000

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints the string in reverse
+ *
+ * @s: input
+ *
+ * Return: Always 0 (success)
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
