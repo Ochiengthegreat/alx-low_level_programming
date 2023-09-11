6dc2d20d4b4bfbf31ef29599ba16aec8b5901ca8
Ochiengthegreat <ochiengroy49@gmail.com>
2023-09-11 17:09:29 +0000

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a struct shows a dog's information
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: My first Structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
